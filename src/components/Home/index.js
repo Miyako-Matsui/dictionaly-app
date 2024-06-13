b2187f8f8fd7e6a84f15c826d52364b97cbe47b3
Miyako Matsui <miyako.matsui18@gmail.com>
2024-06-13 15:45:38 +1200

    add styling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,41 @@
 //home screan
 
-// let count = 0
+import { Box, Typography, FilledInput, IconButton } from '@mui/material'
+
+import {
+  Search as SearchIcon,
+  Bookmark as BookmarkIcon,
+} from '@mui/icons-material'
 
 const Home = () => {
-  // count++
-  // console.log(`count${count}`)
   return (
     <div>
-      <div>HOME</div>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
+      >
+        <img src="/assets/book.png" alt="Book" />
+        <Typography
+          color="primary"
+          sx={{
+            mt: 3,
+            mb: 1,
+          }}
+          variant="h4"
+        >
+          Dictionary
+        </Typography>
+        <Typography>全ア</Typography>
+        <FilledInput />
+        <IconButton>
+          <BookmarkIcon />
+        </IconButton>
+      </Box>
     </div>
   )
 }
--- src/theme.js
+++ src/theme.js
@@ -5,5 +5,15 @@ export default createTheme({
     background: {
       default: '#F1F3F4',
     },
+    primary: {
+      main: '#14194C',
+    },
+  },
+
+  typography: {
+    fontFamily: 'Mulish, sans-serif',
+    h4: {
+      fontWeight: 800,
+    },
   },
 })
